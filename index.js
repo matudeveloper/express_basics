4e549db5fc847a1359ae7f37b9667b91a9ac93aa
Martin Laan <martin.laan@khk.ee>
2022-04-23 17:45:10 +0300

    post request use

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const path = require('path');
 const app = express();
 
@@ -7,7 +8,8 @@ app.use(bodyParser.urlencoded({extend:true}));
 // add directory to public folder
 app.use(express.static('public'));
 //add views directory path
-@@ -10,6 +13,23 @@ app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
+// add views template engine
 app.set('view engine', 'ejs');
 
 
@@ -35,29 +37,36 @@ app.get('/questions', (req, res) => {
     ]
     res.render('index', {questions:questions})
 });
+
 // set up brauser aadress row router
 app.get('/', (req, res) => {
     //create html response
     res.send('<a href="/contact"> Contact us </a> <br> <a href = "/about"> About us </a>');
 });
+
 app.get('/contact', (req, res) => {
     res.send('<h1> Contact us page </h1>');
 });
+
 app.get('/about', (req, res) => {
     res.send('<h1> About us page </h1>');
 });
+
 app.get('/demo', (req, res) => {
     res.render('index.ejs');
 });
+
 app.get('/user/:username', (req, res) => {
     // get parameter data from address row
     let user = req.params.username;
     // use this data template
     res.render('index', {username : user});
 });
+
 app.get('*', (req, res) => {
     res.send('404. This page does not exist. <a href="/"> Go to HomePage </a>');
 });
+
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000');
 });
